77cabfdeaa8956ad497cf6e990edd2fd69be9055
Sr2328 <yadavsachin9311@gmail.com>
2026-01-24 11:14:13 +0530

    fix: Add overflow handling to Layout and reorganize Index component structure for better layout management

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,42 +1,58 @@
 import { Layout } from "@/components/layout/Layout";
-import HeroSection  from "@/components/home/HeroSection";
-import  ServicesSection  from "@/components/home/ServicesSection";
-import  FeaturedJobsSection  from "@/components/home/FeaturedJobsSection";
-import { TestimonialsSection } from "@/components/home/TestimonialsSection";
-import { CTASection } from "@/components/home/CTASection";
+
+import HeroSection from "@/components/home/HeroSection";
 import IndustrySolutionsSection from "@/components/home/IndustrySolutionsSection";
 import ManpowerServices from "@/components/home/ManpowerServices";
-import  ManpowerCategoriesSection  from "@/components/home/ManpowerCategoriesSection";
-import  RequirementBuilderSection  from "@/components/home/RequirementBuilderSection";
-import  ComplianceSection  from "@/components/home/ComplianceSection";
-import TrainingSection  from "@/components/home/TrainingSection";
+import ManpowerCategoriesSection from "@/components/home/ManpowerCategoriesSection";
+import RequirementBuilderSection from "@/components/home/RequirementBuilderSection";
+import TrustSection from "@/components/home/TrustSection";
+import ComplianceSection from "@/components/home/ComplianceSection";
 import { JobDiscoverySection } from "@/components/home/JobDiscoverySection";
+import TrainingSection from "@/components/home/TrainingSection";
 import { SLASection } from "@/components/home/SLASection";
 import { ImpactStatsSection } from "@/components/home/ImpactStatsSection";
+import ServicesSection from "@/components/home/ServicesSection";
+import FeaturedJobsSection from "@/components/home/FeaturedJobsSection";
+import { TestimonialsSection } from "@/components/home/TestimonialsSection";
+import { CTASection } from "@/components/home/CTASection";
 import { ESGSection } from "@/components/home/ESGSection";
 import { FinalCTASection } from "@/components/home/FinalCTASection";
-import  TrustSection  from "@/components/home/TrustSection";
 
 const Index = () => {
   return (
     <Layout>
-      <HeroSection />
-      <IndustrySolutionsSection></IndustrySolutionsSection>
-      <ManpowerServices/>
-       <ManpowerCategoriesSection />
-      <RequirementBuilderSection />
-      <TrustSection />
-      <ComplianceSection />
-      <JobDiscoverySection />
-      <TrainingSection />
-      <SLASection />
-      <ImpactStatsSection />
-      <ServicesSection />
-      <FeaturedJobsSection />
-      <TestimonialsSection />
-      <CTASection />
-      <ESGSection />
-      <FinalCTASection />
+      <div className="overflow-hidden">
+        {/* Hero & Value Proposition */}
+        <HeroSection />
+
+        {/* Solutions & Offerings */}
+        <IndustrySolutionsSection />
+        <ManpowerServices />
+        <ManpowerCategoriesSection />
+
+        {/* Engagement & Trust */}
+        <RequirementBuilderSection />
+        <TrustSection />
+        <ComplianceSection />
+
+        {/* Job & Talent */}
+        <JobDiscoverySection />
+        <TrainingSection />
+        <FeaturedJobsSection />
+
+        {/* Proof & Performance */}
+        <SLASection />
+        <ImpactStatsSection />
+        <TestimonialsSection />
+
+        {/* Services & Sustainability */}
+        <ServicesSection />
+        <ESGSection />
+
+        {/* Calls to Action */}
+        <CTASection />
+        <FinalCTASection />
+      </div>
     </Layout>
   );
 };
